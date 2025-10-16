60f1a7c04d267d8affeecf2c3d983096b6abc2a8
Karthikeyan <168266231+Karthikeyanfullstack@users.noreply.github.com>
2025-10-16 18:30:39 +0530

    Add files via upload

---
+++ Simpleweb/src/test/java/com/fita/Simpleweb/SimplewebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fita.Simpleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplewebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
